fb178b387f3c067a4ac80e414790a21168244182
Tatychka <tatychka-git@yamdex.ru>
2020-12-11 01:43:47 +0800

    Второе ДЗ

---
+++ DZ_2.py
@@ -0,0 +1,11 @@
+import requests
+
+import bs4
+
+class MagnitParse:
+    def __init__(self,start_url):
+        self.start_url=start_url
+
+    @staticmethod
+    def _get(self, *args, **kwargs):
+
