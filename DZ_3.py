95bc1f9b72fd58f5fb4832f9628957c74bc9d476
Tatychka <tatychka-git@yamdex.ru>
2020-12-17 21:38:27 +0800

    Домашняя работа №3

--- DZ_3.py
+++ DZ_3.py
@@ -1,1 +1,32 @@
-import
+from typing import Tuple
+import bs4
+import requests
+from urllib.parse import urljoin
+
+
+class GbBlogParse:
+
+    def __init__(self, start_url):
+        self.start_url = start_url
+        self.page_done = set()
+
+    def _get(self, url):
+        response = requests.get(url)
+        self.page_done.add(url)
+        return bs4.BeautifulSoup(response.text, 'lxml')
+
+    def run(self, url=None):
+        if not url:
+            url = self.start_url
+
+        if url not in self.page_done:
+            soup = self._get(url)
+            posts, pagination = self.parse(soup)
+
+    def parse(self, soup):
+        print(1)
+
+
+if __name__ ==  '__main__':
+    parser = GbBlogParse('https://geekbrains.ru/posts')
+    parser.run()
